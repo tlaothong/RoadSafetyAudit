3844da047ad078979129162fa7584a403bdf15bd
Teerachai Laothong <tlaothong@gmail.com>
2018-04-17 09:39:07 +0700

    Toggle sub items.

--- src/pages/project-check-list-l0/project-check-list-l0.ts
+++ src/pages/project-check-list-l0/project-check-list-l0.ts
@@ -10,33 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-project-check-list-l0',
-  templateUrl: 'project-check-list-l0.html',
+	selector: 'page-project-check-list-l0',
+	templateUrl: 'project-check-list-l0.html',
 })
 export class ProjectCheckListL0Page {
 
-  public checkLists;
-  public l1page = 'ProjectCheckListL1Page';
-  public checkByTitle;
+	public checkLists;
+	public l1page = 'ProjectCheckListL1Page';
+	public checkByTitle;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    var checkList = this.navParams.data;
-    this.checkByTitle = checkList.code == 'it'? "ตรวจสอบจากระบบสารสนเทศ": "ตรวจสอบจากงานภาคสนาม";
-    this.checkLists = checkList.items;
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+		var checkList = this.navParams.data;
+		this.checkByTitle = checkList.code == 'it'? "ตรวจสอบจากระบบสารสนเทศ": "ตรวจสอบจากงานภาคสนาม";
+		this.checkLists = checkList.items;
+	}
 
-  public goL1Page(l0) {
-    this.navCtrl.push(this.l1page, { l0: l0, checkBy: this.checkByTitle });
-  }
+	public goL1Page(l0) {
+		this.navCtrl.push(this.l1page, { l0: l0, checkBy: this.checkByTitle });
+	}
 
-  public toggleSubItems(item) {
-    item.open = !item.open;
-  }
+	public toggleSubItems(item) {
+		if (item.showSubItems) {
+			item.open = !item.open;
+		} else {
+			this.goL1Page(item);
+		}
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProjectCheckListL0Page');
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad ProjectCheckListL0Page');
 
-    // this.checkLists = this.chkList.getCheckList('123v1');
-  }
+		// this.checkLists = this.chkList.getCheckList('123v1');
+	}
 
 }
